656716d98344dc3edd2507c9f5a6fac16c96dc3e
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-11-21 14:38:18 +0530

    first commit

--- converter.js
+++ converter.js
@@ -2,17 +2,13 @@ const imgToPDF = require("image-to-pdf");
 const fs = require("fs");
 const path = require("path");
 const upload_path = path.join(__dirname, "./uploads/");
+
 const converter = (filepath) => {
   const pages = [
     `${upload_path}${filepath}`, // path to the image
-    // "data:image/jpeg;base64,iVBORw...", // base64
     fs.readFileSync(`${upload_path}${filepath}`), // Buffer
   ];
 
-  imgToPDF(pages, imgToPDF.sizes.A4).pipe(fs.createWriteStream(`output.pdf`));
+  imgToPDF(pages, imgToPDF.sizes.A4).pipe(fs.createWriteStream("output.pdf"));
 };
 module.exports = { converter };
-
-
-
-
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const { converter } = require("./converter");
 const imgToPDF = require("image-to-pdf");
+const fs = require("fs");
 // const fs = require("fs");
 const out_path = path.join(__dirname, "/output.pdf");
 app.use(express.static("./public"));
@@ -13,13 +14,16 @@ app.use(express.static("./public"));
 app.get("/", (req, res) => {
   res.send("index");
 });
-
-app.post("/converter", upload.array("image", 1), (req, res, next) => {
+app.get("/converter", (req, res) => {
+  res.sendFile(out_path);
+});
+app.post("/converter", upload.array("image", 1), async (req, res, next) => {
   if (req.files.length === 0) {
     res.status(400).send("Files not uploaded");
   } else {
-    converter(req.files[0].filename);
-    res.status(201).sendFile(out_path);
+    await converter(req.files[0].filename);
+    res.redirect(`http://localhost:${port}/converter`);
+    // res.sendFile(out_path)
   }
 });
 app.listen(port, () => {
