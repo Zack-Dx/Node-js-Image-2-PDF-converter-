28913079d109a9bd1aa1a37efb125ba748a36d96
AJ ONeal <coolaj86@gmail.com>
2022-11-25 11:50:43 -0700

    fix: 1 image per page, wait until completion to respond

--- converter.js
+++ converter.js
@@ -1,14 +1,25 @@
 const imgToPDF = require("image-to-pdf");
-const fs = require("fs");
-const path = require("path");
-const upload_path = path.join(__dirname, "./uploads/");
+const Fs = require("node:fs/promises");
+const FsSync = require("node:fs");
+const Path = require("node:path");
+const upload_path = Path.join(__dirname, "./uploads/");
 
-const converter = (filepath) => {
+async function converter(filepath) {
+  let page = await Fs.readFile(`${upload_path}${filepath}`);
   const pages = [
-    `${upload_path}${filepath}`, // path to the image
-    fs.readFileSync(`${upload_path}${filepath}`), // Buffer
+    page,
   ];
-
-  imgToPDF(pages, imgToPDF.sizes.A4).pipe(fs.createWriteStream("output.pdf"));
+  
+  let rs = Fs.createWriteStream("output.pdf");
+  
+  let p = new Promise(function (resolve, reject) {
+    rs.on('close', resolve);
+    rs.on('end', resolve);
+    rs.on('error', reject);
+  })
+  
+  imgToPDF(pages, imgToPDF.sizes.A4).pipe(rs);
+  
+  return await p;
 };
 module.exports = { converter };
